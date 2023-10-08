57f6b17b1b294ab5aeaee5c23dc705cc41617c33
Raghav Gupta <raghavgupta216@gmail.com>
2023-10-09 00:42:57 +0530

    Add files via upload

---
+++ HTML/Survey Form/script.js
@@ -0,0 +1,25 @@
+document.getElementById('surveyForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var dob = document.getElementById('dob').value;
+    var country = document.getElementById('country').value;
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var profession = document.getElementById('profession').value;
+    var email = document.getElementById('email').value;
+    var mobileNumber = document.getElementById('mobileNumber').value;
+
+    var formValues = 'First Name: ' + firstName + '\n' +
+                     'Last Name: ' + lastName + '\n' +
+                     'Date of Birth: ' + dob + '\n' +
+                     'Country: ' + country + '\n' +
+                     'Gender: ' + gender + '\n' +
+                     'Profession: ' + profession + '\n' +
+                     'Email: ' + email + '\n' +
+                     'Mobile Number: ' + mobileNumber;
+
+    alert(formValues);
+
+    document.getElementById('surveyForm').reset();
+});
