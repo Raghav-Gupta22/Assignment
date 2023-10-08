f648e9fd27450ef9b6d27817af83165302ef8314
Raghav Gupta <raghavgupta216@gmail.com>
2023-10-08 20:20:43 +0530

    Create 2.js

---
+++ Javascript/2.js
@@ -0,0 +1,16 @@
+function bubbleSortDescending(arr) {
+    let len = arr.length;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j < len - i - 1; j++) {
+            if (arr[j] < arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+let array = [1, 3, 5, 2, 4];
+console.log(bubbleSortDescending(array));
